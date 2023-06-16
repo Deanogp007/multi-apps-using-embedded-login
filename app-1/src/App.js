6a0b9de8f37338179e5f9921b1093d8c4928fb4a
Aviad Mizrachi <aviadm@gmail.com>
2023-06-16 11:38:12 +0300

    first commit

---
+++ app-1/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { useAuth } from "@frontegg/react";
+
+function App() {
+  const { user, isAuthenticated } = useAuth();
+
+  const redirectToLogin = () => {
+      window.location.href = `http://localhost:3000?redirectUrl=${window.location}`;
+  }
+  
+  return (
+      <div className='App'>
+        {isAuthenticated ? (
+            <div>
+              <img src={user.profilePictureUrl} alt={user.name} />
+              <span>{user.name}</span>
+            </div>
+        ) : 
+            <div>
+                <button onClick={() => redirectToLogin()}>Click to authenticate</button>
+            </div> 
+        }
+      </div>
+  );
+}
+
+export default App;
